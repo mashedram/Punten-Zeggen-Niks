bf5475b8f8d5cae7316441b5b035c6eccb19887d
Bas Bakker <basbakker1912@gmail.com>
2025-04-08 11:52:43 +0200

    - Added a client query - Changed _layout.tsx to handle the websocket - Added bun for the server

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,10 +3,15 @@ import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { getQueryClient, TRPCProvider } from '@/api/query';
+import { getTrpcClient } from '@/api/client';
+import { createTRPCClient, createWSClient, loggerLink, wsLink } from '@trpc/client';
+import { AppRouter } from '@/api/router/root';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -27,13 +32,38 @@ export default function RootLayout() {
     return null;
   }
 
+  const queryClient = getQueryClient();
+  const [trpcClient] = useState(() => {
+    const wsClient = createWSClient({
+      // TODO: Make this load from env at compile time
+      url: "ws://localhost:3001"
+    })
+
+    return createTRPCClient<AppRouter>({
+      links: [
+          wsLink({
+              client: wsClient
+          }),
+          loggerLink({
+            enabled: (opts) =>
+              process.env.NODE_ENV === "development" ||
+              (opts.direction === "down" && opts.result instanceof Error),
+          }),
+      ],
+  })
+  })
+
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <TRPCProvider queryClient={queryClient} trpcClient={trpcClient}>
+        <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+          <Stack>
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            <Stack.Screen name="+not-found" />
+          </Stack>
+          <StatusBar style="auto" />
+        </ThemeProvider>
+      </TRPCProvider>
+    </QueryClientProvider>
   );
 }
