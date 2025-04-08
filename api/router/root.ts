7dd2946f44fc97bc8789e20ff4a06c3e4bee0a4c
Bas Bakker <basbakker1912@gmail.com>
2025-04-08 11:52:43 +0200

    - Added a client query - Changed _layout.tsx to handle the websocket - Added bun for the server

--- api/router/root.ts
+++ api/router/root.ts
@@ -1,7 +1,19 @@
-import { router } from "@/api/server";
+import { publicProcedure, router } from "@/api/server";
+import { z } from 'zod'
 
 export const appRouter = router({
+    users: publicProcedure.input(z.object({ name: z.string().optional() })).query(({ ctx, input }) => {
+        const users = [
+            {
+                name: "Jan"
+            },
+            {
+                name: "Willem"
+            }
+        ]
 
+        return input.name ? users.filter(v => v.name == input.name) : users
+    })
 })
 
 export type AppRouter = typeof appRouter
--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -6,8 +6,14 @@ import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import { IconSymbol } from '@/components/ui/IconSymbol';
+import { useTRPC } from '@/api/query';
+import { useQuery } from '@tanstack/react-query';
 
 export default function TabTwoScreen() {
+  const trpc = useTRPC()
+
+  const userQuery = useQuery(trpc.users.queryOptions({}))
+
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
@@ -22,6 +28,7 @@ export default function TabTwoScreen() {
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="title">Explore</ThemedText>
       </ThemedView>
+      {!userQuery.isPending && <ThemedText>We got the following response from the server! {userQuery.data[0].name}</ThemedText> || <ThemedText>"Not loaded yet"</ThemedText>}
       <ThemedText>This app includes example code to help you get started.</ThemedText>
       <Collapsible title="File-based routing">
         <ThemedText>
