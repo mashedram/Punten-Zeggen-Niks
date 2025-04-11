d9a0ae7f42396c930e1a1e7a5644ce2060d56d0b
Bas Bakker <basbakker1912@gmail.com>
2025-04-11 14:49:49 +0200

    feat: fixed prettier config

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,9 +5,10 @@ import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import { usePlayerData } from '@/hooks/usePlayerData';
+import { useRef } from 'react';
 
 export default function HomeScreen() {
-  const [playerdata, joinLobby] = usePlayerData()
+  const [playerdata] = usePlayerData()
 
   return (
     <ParallaxScrollView
