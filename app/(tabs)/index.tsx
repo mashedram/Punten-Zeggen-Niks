00f0c595077e4ab8ee51efe31e86c01765d722b2
Bas Bakker <basbakker1912@gmail.com>
2025-04-11 10:21:58 +0200

    Added: - Lobby logic - tRPC API - NFC Reader start

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,16 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Image, StyleSheet, Platform, TextInput } from 'react-native';
 
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import { usePlayerData } from '@/hooks/usePlayerData';
+import { useRef } from 'react';
+import { Test } from '@/components/Test';
 
 export default function HomeScreen() {
+  const [playerdata, joinLobby] = usePlayerData()
+
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
@@ -15,10 +20,12 @@ export default function HomeScreen() {
           style={styles.reactLogo}
         />
       }>
+        <Test callback={(value) => console.log(value)}></Test>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+        <ThemedText type="title">Welcome! + {playerdata?.id}</ThemedText>
         <HelloWave />
       </ThemedView>
+      <TextInput></TextInput>
       <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle">Step 1: Try it</ThemedText>
         <ThemedText>
