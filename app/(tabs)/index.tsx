5a53d24b21802c7f733c7c08ff552558b46b94b1
Bas Bakker <basbakker1912@gmail.com>
2025-04-11 14:49:49 +0200

    feat: fixed prettier config

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,7 +6,6 @@ import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import { usePlayerData } from '@/hooks/usePlayerData';
 import { useRef } from 'react';
-import { Test } from '@/components/Test';
 
 export default function HomeScreen() {
   const [playerdata, joinLobby] = usePlayerData()
@@ -20,7 +19,6 @@ export default function HomeScreen() {
           style={styles.reactLogo}
         />
       }>
-        <Test callback={(value) => console.log(value)}></Test>
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="title">Welcome! + {playerdata?.id}</ThemedText>
         <HelloWave />
