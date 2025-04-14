b463a2cb675f5f9d65212f6ce557f873fc6fe938
Bas Bakker <basbakker1912@gmail.com>
2025-04-14 11:45:44 +0200

    feat: Add formatting for consistency purposes. feat: Added lobby garbage collection when they're empty feat: Added automatic player connection state management using the state of the websocket that handles events. feat: Changed lobbies to use react contexts for global state management.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,26 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect, useState } from 'react';
-import 'react-native-reanimated';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import "react-native-reanimated";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { createTRPCClient, createWSClient, loggerLink, wsLink } from '@trpc/client';
-import { AppRouter } from '@/api/router/root';
-import { TRPCProvider } from '@/api/query';
+import { useColorScheme } from "@/hooks/useColorScheme";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  createTRPCClient,
+  createWSClient,
+  loggerLink,
+  wsLink,
+} from "@trpc/client";
+import { AppRouter } from "@/api/router/root";
+import { TRPCProvider } from "@/api/query";
+import { LobbyProvider } from "@/hooks/useLobby";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -26,7 +36,7 @@ const queryClient = new QueryClient({
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
 
   useEffect(() => {
@@ -40,13 +50,12 @@ export default function RootLayout() {
     const wsClient = createWSClient({
       // TODO: Make this load from env at compile time
       url: "ws://100.80.93.96:3001",
-    })
-
+    });
 
     return createTRPCClient<AppRouter>({
       links: [
         wsLink({
-          client: wsClient
+          client: wsClient,
         }),
         loggerLink({
           enabled: (opts) =>
@@ -54,8 +63,8 @@ export default function RootLayout() {
             (opts.direction === "down" && opts.result instanceof Error),
         }),
       ],
-    })
-  })
+    });
+  });
 
   if (!loaded) {
     return null;
@@ -64,12 +73,16 @@ export default function RootLayout() {
   return (
     <QueryClientProvider client={queryClient}>
       <TRPCProvider queryClient={queryClient} trpcClient={trpcClient}>
-        <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-          <Stack>
-            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-            <Stack.Screen name="+not-found" />
-          </Stack>
-          <StatusBar style="auto" />
+        <ThemeProvider
+          value={colorScheme === "dark" ? DarkTheme : DefaultTheme}
+        >
+          <LobbyProvider>
+            <Stack>
+              <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+              <Stack.Screen name="+not-found" />
+            </Stack>
+            <StatusBar style="auto" />
+          </LobbyProvider>
         </ThemeProvider>
       </TRPCProvider>
     </QueryClientProvider>
