dd959228a3a5fed5ecaec5f49dff10de837da46d
Niels van Schaik <nielsvanschaik14@gmail.com>
2025-05-30 12:08:02 +0200

    fix: if statement bomb

--- api/managers/statego/StrategoGame.ts
+++ api/managers/statego/StrategoGame.ts
@@ -2,10 +2,11 @@ import { GameType } from '@/api/game/GameType';
 import { z } from 'zod';
 import { Lobby } from '../lobby/Lobby';
 import { Player } from '../lobby/Player';
-import { RoleCard, RoleCards } from '../../../constants/RoleCards';
+import { RoleCards } from '../../../constants/RoleCards';
 import { GameState } from '../../../constants/GameState';
 import { createRoleCardDeck } from '@/constants/RoleCardDeck';
 import { console } from 'inspector';
+import { performAttack } from './AttackFunctions';
 
 export const StrategoGameId = 'stratego';
 
@@ -89,53 +90,6 @@ export const GameTypeStratego: GameType<PlayerDataStratego, LobbyDataStratego> =
 /// FUNCTIONS ///
 /////////////////
 
-function getLobbyData(lobby: Lobby): LobbyDataStratego {
-  return lobby.getGameData<LobbyDataStratego>();
-}
-
-function getPlayerData(player: Player): PlayerDataStratego {
-  return player.getGameData<PlayerDataStratego>();
-}
-
-function getRoleCard(cardId?: string): RoleCard | undefined {
-  console.log(`Getting role card for cardId: ${cardId}`);
-  if (cardId === undefined) {
-    console.warn(`Role card with id ${cardId} not found.`);
-    return undefined;
-  }
-
-  const RoleCard = RoleCards[cardId];
-  if (!RoleCard) {
-    console.warn(`Role card with id ${cardId} not found.`);
-    return undefined;
-  }
-  return RoleCards[cardId];
-}
-
-function getRoleCardFromSelection(
-  lobby: Lobby,
-  teamId: string,
-): string | undefined {
-  const lobbyData = getLobbyData(lobby);
-  const team = lobbyData.teams.find(team => team.id === teamId);
-  if (!team || !team.deck) {
-    throw new Error(`Team ${teamId} or its deck not found in lobby data.`);
-  }
-  const availableCards = Object.keys(team.deck);
-  console.log(`Available cards for team ${teamId}:`, team.deck);
-  if (team.deck[RoleCards.vlag.name] > 0) {
-    removeRoleCardFromDeck(lobby, teamId, RoleCards.vlag.name);
-    return RoleCards.vlag.name;
-  }
-  if (availableCards.length === 0) {
-    return undefined;
-  }
-  const cardName =
-    availableCards[Math.floor(Math.random() * availableCards.length)];
-  removeRoleCardFromDeck(lobby, teamId, cardName);
-  return cardName;
-}
-
 function removeRoleCardFromDeck(lobby: Lobby, teamId: string, cardId: string) {
   const lobbyData = getLobbyData(lobby);
   const team = lobbyData.teams.find(team => team.id === teamId);
@@ -195,203 +149,6 @@ function getNewPlayerTeam(lobby: Lobby): string {
   return smallestTeam;
 }
 
-////////////////////////
-/// Attack functions ///
-////////////////////////
-
-function performAttack(lobby: Lobby, attacker: Player, defender: Player) {
-  const attackerData = getPlayerData(attacker);
-  const defenderData = getPlayerData(defender);
-  if (!attackerData || !defenderData) {
-    console.warn('Player data not found for attacker or defender.');
-    return;
-  }
-  const attackerCard = getRoleCard(attackerData.roleCard);
-  const defenderCard = getRoleCard(defenderData.roleCard);
-  console.log(
-    `Performing attack: ${attacker.getId()} ${attackerData.roleCard} vs ${defender.getId()} ${defenderData.roleCard}`,
-  );
-
-  // check for team conflict
-  if (attackerData.teamId === defenderData.teamId) {
-    console.warn('Cannot attack a player from the same team.');
-    return;
-  }
-  // check if game is still active
-  if (getLobbyData(lobby).gameState !== String(GameState.playing)) {
-    console.warn('Game is not active, cannot perform attack.');
-    return;
-  }
-
-  // no role card for attacker or defender
-  if (!attackerCard) {
-    console.warn('Attacker does not have a role card.', attackerCard);
-    return;
-  }
-  if (!defenderCard) {
-    console.warn('Defender does not have a role card.', defenderCard);
-    return;
-  }
-
-  // check if defender has a flag
-  // TODO: vlag kan niet aanvallen, maar kan wel verdedigen
-  if (defenderCard.name === RoleCards.vlag.name) {
-    endGame(lobby, attackerData.teamId);
-    console.log(
-      `Team ${attackerData.teamId} has captured the flag of team ${defenderData.teamId}.`,
-    );
-    return;
-  }
-  // spion verslaat maarschalk
-  // TODO???: spion verliest als hij aangevallen wordt door maarschalk
-  if (
-    attackerCard.name === RoleCards.spion.name &&
-    defenderCard.name === RoleCards.maarschalk.name
-  ) {
-    console.log('Spion vs Maarschalk detected. Spion wins.');
-    win(attacker);
-    defeat(defender);
-    return;
-  }
-  if (
-    defenderCard.name === RoleCards.spion.name &&
-    attackerCard.name === RoleCards.maarschalk.name
-  ) {
-    console.log('Maarschalk vs Spion detected. Spion wins.');
-    win(defender);
-    defeat(attacker);
-    return;
-  }
-
-  // TODO: bom kan niet aanvallen, maar kan wel verdedigen
-  if (
-    attackerCard.name === RoleCards.bom.name ||
-    defenderCard.name === RoleCards.bom.name
-  ) {
-    console.log(
-      'Bom detected in the fight. attacker:',
-      attackerCard.name,
-      'defender:',
-      defenderCard.name,
-    );
-    if (
-      attackerCard.name === RoleCards.mineur.name ||
-      defenderCard.name !== RoleCards.mineur.name
-    ) {
-      if (attackerCard.name === RoleCards.mineur.name) {
-        win(attacker);
-        defeat(defender);
-        console.log('Attacker wins, mineur defuses bom');
-        return;
-      }
-      if (defenderCard.name === RoleCards.mineur.name) {
-        win(defender);
-        defeat(attacker);
-        console.log('Defender wins, mineur defuses bom');
-        return;
-      }
-    }
-
-    if (
-      attackerCard.name === RoleCards.bom.name &&
-      defenderCard.name === RoleCards.bom.name
-    ) {
-      explode(attacker);
-      explode(defender);
-      console.log('Both players explode');
-      return;
-    }
-    if (attackerCard.name === RoleCards.bom.name) {
-      explode(attacker);
-      defeat(defender);
-      console.log('Attacker loses, bom explodes');
-      return;
-    }
-    if (defenderCard.name === RoleCards.bom.name) {
-      explode(defender);
-      defeat(attacker);
-      console.log('Defender loses, bom explodes');
-      return;
-    }
-  }
-
-  // Atacker wins
-  if (attackerCard.value > defenderCard.value) {
-    console.log(
-      `Attacker wins: ${attackerCard.name} (${attackerCard.value}) vs ${defenderCard.name} (${defenderCard.value})`,
-    );
-    win(attacker);
-    defeat(defender);
-    return;
-  }
-
-  // Defender wins
-  if (attackerCard.value < defenderCard.value) {
-    console.log(
-      `Defender wins: ${defenderCard.name} (${defenderCard.value}) vs ${attackerCard.name} (${attackerCard.value})`,
-    );
-    win(defender);
-    defeat(attacker);
-    return;
-  }
-
-  // Both cards have the same value, resulting in a draw
-  // TODO: Implement edgecases
-  console.log(
-    `Both players have the same card value: ${attackerCard.value}. It's a draw.`,
-  );
-  draw(attacker);
-  draw(defender);
-}
-
-function draw(player: Player) {
-  const data = getPlayerData(player);
-  data.lastFightResult = {
-    index: data.lastFightResult ? data.lastFightResult.index + 1 : 0,
-    state: 'draw',
-  };
-  player.sync();
-}
-
-function win(player: Player) {
-  const data = getPlayerData(player);
-  data.lastFightResult = {
-    index: data.lastFightResult ? data.lastFightResult.index + 1 : 0,
-    state: 'win',
-  };
-  player.sync();
-}
-
-function defeat(player: Player) {
-  const playerData = getPlayerData(player);
-  playerData.roleCard = getRoleCardFromSelection(
-    player.getLobby(),
-    playerData.teamId,
-  );
-  playerData.lastFightResult = {
-    index: playerData.lastFightResult
-      ? playerData.lastFightResult.index + 1
-      : 0,
-    state: 'lose',
-  };
-  player.sync();
-}
-
-function explode(player: Player) {
-  const playerData = getPlayerData(player);
-  playerData.roleCard = getRoleCardFromSelection(
-    player.getLobby(),
-    playerData.teamId,
-  );
-  playerData.lastFightResult = {
-    index: playerData.lastFightResult
-      ? playerData.lastFightResult.index + 1
-      : 0,
-    state: 'explode',
-  };
-  player.sync();
-}
-
 function checkWinConditions(lobby: Lobby) {
   const lobbyData = getLobbyData(lobby);
   const teams = lobbyData.teams;
@@ -406,7 +163,47 @@ function checkWinConditions(lobby: Lobby) {
   }
 }
 
-function endGame(lobby: Lobby, winningTeamId: string) {
+// Returns true if there are active platers with a role card in the given team
+function checkActiveRoleCards(lobby: Lobby, teamId: string): boolean {
+  const players = lobby
+    .getActivePlayers()
+    .filter(
+      player => player.getGameData<PlayerDataStratego>().teamId === teamId,
+    );
+  return players.some(
+    player => player.getGameData<PlayerDataStratego>().roleCard === undefined,
+  );
+}
+
+//////////////////////
+/// External utils ///
+//////////////////////
+
+export function getRoleCardFromSelection(
+  lobby: Lobby,
+  teamId: string,
+): string | undefined {
+  const lobbyData = getLobbyData(lobby);
+  const team = lobbyData.teams.find(team => team.id === teamId);
+  if (!team || !team.deck) {
+    throw new Error(`Team ${teamId} or its deck not found in lobby data.`);
+  }
+  const availableCards = Object.keys(team.deck);
+  console.log(`Available cards for team ${teamId}:`, team.deck);
+  if (team.deck[RoleCards.vlag.name] > 0) {
+    removeRoleCardFromDeck(lobby, teamId, RoleCards.vlag.name);
+    return RoleCards.vlag.name;
+  }
+  if (availableCards.length === 0) {
+    return undefined;
+  }
+  const cardName =
+    availableCards[Math.floor(Math.random() * availableCards.length)];
+  removeRoleCardFromDeck(lobby, teamId, cardName);
+  return cardName;
+}
+
+export function endGame(lobby: Lobby, winningTeamId: string) {
   const lobbyData = getLobbyData(lobby);
   if (winningTeamId === 'red') {
     lobbyData.gameState = String(GameState.red_wins);
@@ -421,21 +218,17 @@ function endGame(lobby: Lobby, winningTeamId: string) {
   console.log(`Game ended. Team ${winningTeamId} has won.`);
 }
 
-// Returns true if there are active platers with a role card in the given team
-function checkActiveRoleCards(lobby: Lobby, teamId: string): boolean {
-  const players = lobby
-    .getActivePlayers()
-    .filter(
-      player => player.getGameData<PlayerDataStratego>().teamId === teamId,
-    );
-  return players.some(
-    player => player.getGameData<PlayerDataStratego>().roleCard === undefined,
-  );
-}
-
 //////////////////////////
 /// EXTERNAL FUNCTIONS ///
 //////////////////////////
+
+export function getLobbyData(lobby: Lobby): LobbyDataStratego {
+  return lobby.getGameData<LobbyDataStratego>();
+}
+
+export function getPlayerData(player: Player): PlayerDataStratego {
+  return player.getGameData<PlayerDataStratego>();
+}
 
 export const StrategoGame = {
   attack(lobby: Lobby, attacker: Player, attackCode: string) {
--- constants/RoleCards.ts
+++ constants/RoleCards.ts
@@ -1,64 +1,156 @@
 export type RoleCard = {
   name: string;
   value: number;
+  canAttack: boolean;
+  beats: RoleCard[];
   image?: string;
   overwrites?: string[];
 };
 
+const vlag: RoleCard = {
+  name: 'vlag',
+  value: 0,
+  image: require('@/assets/images/Vlag.png'),
+  canAttack: false,
+  beats: [],
+};
+
+const maarschalk: RoleCard = {
+  name: 'maarschalk',
+  value: 10,
+  image: require('@/assets/images/Maarschalk.png'),
+  canAttack: true,
+  beats: [],
+};
+
+const generaal: RoleCard = {
+  name: 'generaal',
+  value: 9,
+  image: require('@/assets/images/Generaal.png'),
+  canAttack: true,
+  beats: [],
+};
+
+const kolonel: RoleCard = {
+  name: 'kolonel',
+  value: 8,
+  image: require('@/assets/images/Kolonel.png'),
+  canAttack: true,
+  beats: [],
+};
+
+const majoor: RoleCard = {
+  name: 'majoor',
+  value: 7,
+  image: require('@/assets/images/Majoor.png'),
+  canAttack: true,
+  beats: [],
+};
+
+const kapitein: RoleCard = {
+  name: 'kapitein',
+  value: 6,
+  image: require('@/assets/images/Kapitein.png'),
+  canAttack: true,
+  beats: [],
+};
+
+const luitenant: RoleCard = {
+  name: 'luitenant',
+  value: 5,
+  image: require('@/assets/images/Luitenant.png'),
+  canAttack: true,
+  beats: [],
+};
+
+const sergeant: RoleCard = {
+  name: 'sergeant',
+  value: 4,
+  image: require('@/assets/images/Sergeant.png'),
+  canAttack: true,
+  beats: [],
+};
+
+const mineur: RoleCard = {
+  name: 'mineur',
+  value: 3,
+  image: require('@/assets/images/Mineur.png'),
+  canAttack: true,
+  beats: [],
+};
+
+const spion: RoleCard = {
+  name: 'spion',
+  value: 2,
+  image: require('@/assets/images/Spion.png'),
+  canAttack: true,
+  beats: [],
+};
+
+const bom: RoleCard = {
+  name: 'bom',
+  value: 1,
+  image: require('@/assets/images/Bom.png'),
+  canAttack: false,
+  beats: [],
+};
+
+maarschalk.beats = [
+  generaal,
+  kolonel,
+  majoor,
+  kapitein,
+  luitenant,
+  sergeant,
+  mineur,
+  spion,
+];
+
+generaal.beats = [
+  kolonel,
+  majoor,
+  kapitein,
+  luitenant,
+  sergeant,
+  mineur,
+  spion,
+];
+
+kolonel.beats = [majoor, kapitein, luitenant, sergeant, mineur, spion];
+
+majoor.beats = [kapitein, luitenant, sergeant, mineur, spion];
+
+kapitein.beats = [luitenant, sergeant, mineur, spion];
+
+luitenant.beats = [sergeant, mineur, spion];
+
+sergeant.beats = [mineur, spion];
+
+mineur.beats = [spion, bom];
+
+spion.beats = [maarschalk];
+
+bom.beats = [
+  maarschalk,
+  generaal,
+  kolonel,
+  majoor,
+  kapitein,
+  luitenant,
+  sergeant,
+  spion,
+];
+
 export const RoleCards: Record<string, RoleCard> = {
-  vlag: {
-    name: 'vlag',
-    value: 0,
-    image: require('@/assets/images/Vlag.png'),
-  },
-  maarschalk: {
-    name: 'maarschalk',
-    value: 10,
-    image: require('@/assets/images/Maarschalk.png'),
-  },
-  generaal: {
-    name: 'generaal',
-    value: 9,
-    image: require('@/assets/images/Generaal.png'),
-  },
-  kolonel: {
-    name: 'kolonel',
-    value: 8,
-    image: require('@/assets/images/Kolonel.png'),
-  },
-  majoor: {
-    name: 'majoor',
-    value: 7,
-    image: require('@/assets/images/Majoor.png'),
-  },
-  kapitein: {
-    name: 'kapitein',
-    value: 6,
-    image: require('@/assets/images/Kapitein.png'),
-  },
-  luitenant: {
-    name: 'luitenant',
-    value: 5,
-    image: require('@/assets/images/Luitenant.png'),
-  },
-  sergeant: {
-    name: 'sergeant',
-    value: 4,
-    image: require('@/assets/images/Sergeant.png'),
-  },
-  mineur: {
-    name: 'mineur',
-    value: 3,
-    image: require('@/assets/images/Mineur.png'),
-  },
-  spion: {
-    name: 'spion',
-    value: 2,
-    image: require('@/assets/images/Spion.png'),
-  },
-  bom: {
-    name: 'bom',
-    value: 1,
-    image: require('@/assets/images/Bom.png'),
-  },
+  vlag,
+  maarschalk,
+  generaal,
+  kolonel,
+  majoor,
+  kapitein,
+  luitenant,
+  sergeant,
+  mineur,
+  spion,
+  bom,
 };
