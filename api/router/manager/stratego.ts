dca33d635a821bed976817c9b7ae5d5bb6fe2cb7
Niels van Schaik <nielsvanschaik14@gmail.com>
2025-05-30 11:33:13 +0200

    feat: added a way for the client to check rivive and get available cards in deck

--- api/router/manager/stratego.ts
+++ api/router/manager/stratego.ts
@@ -2,6 +2,7 @@ import { lobbyManager } from '@/api/managers/lobby/LobbyManager';
 import { PlayerToken } from '@/api/managers/lobby/PlayerToken';
 import { StrategoGame } from '@/api/managers/statego/StrategoGame';
 import { publicProcedure, router } from '@/api/server';
+import { RoleCards } from '@/constants/RoleCards';
 import { z } from 'zod';
 
 export const strategoRouter = router({
@@ -14,5 +15,38 @@ export const strategoRouter = router({
       if (!entity) throw new Error('Lobby not found');
       const [lobby, attacker] = entity;
       StrategoGame.attack(lobby, attacker, input.attackCode);
+    }),
+  revive: publicProcedure
+    .input(
+      z.object({
+        token: z.string(),
+        targetId: z.string(),
+        roleCard: z.string(),
+      }),
+    )
+    .mutation(({ input }) => {
+      const roleCard = RoleCards[input.roleCard];
+      if (!roleCard) throw new Error('Invalid role card');
+      const entity = lobbyManager.getPlayer(
+        PlayerToken.fromString(input.token),
+      );
+      if (!entity) throw new Error('Lobby not found');
+      const [lobby, teamLeader] = entity;
+      const targetPlayer = lobby.getPlayer(
+        PlayerToken.fromString(input.targetId),
+      );
+      if (!targetPlayer) throw new Error('Target player not found');
+      StrategoGame.revive(lobby, teamLeader, targetPlayer, roleCard);
+    }),
+  getAvailableRoleCards: publicProcedure
+    .output(z.record(z.string(), z.number()))
+    .input(z.object({ token: z.string() }))
+    .query(({ input }) => {
+      const entity = lobbyManager.getPlayer(
+        PlayerToken.fromString(input.token),
+      );
+      if (!entity) throw new Error('Lobby not found');
+      const [lobby, player] = entity;
+      return StrategoGame.getAvailableRoleCards(lobby, player);
     }),
 });
