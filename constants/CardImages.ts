7218f799493115ec77ea4ac0c6d9b3ae0e67d9cf
J3no4h <jenoah310@hotmail.com>
2025-06-05 10:51:43 +0200

    fix: cardimages kloppend gemaakt

--- constants/CardImages.ts
+++ constants/CardImages.ts
@@ -6,11 +6,11 @@ export const CardImages: Record<string, ImageSourcePropType> = {
   luitenant: require('@/assets/images/Luitenant.png'),
   kapitein: require('@/assets/images/Kapitein.png'),
   kolonel: require('@/assets/images/Kolonel.png'),
-  adjudant: require('@/assets/images/Adjudant.png'),
   mineur: require('@/assets/images/Mineur.png'),
   vlag: require('@/assets/images/Vlag.png'),
   general: require('@/assets/images/Generaal.png'),
   verkenner: require('@/assets/images/Verkenner.png'),
   sergeant: require('@/assets/images/Sergeant.png'),
   bom: require('@/assets/images/Bom.png'),
+  maarschalk: require('@/assets/images/Maarschalk.png'),
 };
