e4f0a8b31100723a65434395fb592ae202ff58be
Niels van Schaik <nielsvanschaik14@gmail.com>
2025-06-10 12:49:17 +0200

    fix: temp removal of powerups and fixing css

--- app/stratego/index.tsx
+++ app/stratego/index.tsx
@@ -1,6 +1,5 @@
 import { useTRPC } from '@/api/query';
 import { PlayerRole } from '@/components/Spelersrollen/PlayerRole';
-import { PowerUpPopUp } from '@/components/ui/PowerUpPopUp';
 import {
   InitalizationFailureReason,
   useStratego,
@@ -14,15 +13,12 @@ import {
   SafeAreaView,
   Text,
   TextInput,
-  Image,
   StyleSheet,
   View,
-  Pressable,
   Platform,
   StatusBar,
 } from 'react-native';
 import Svg, { Circle } from 'react-native-svg';
-import { PowerUpList } from '@/constants/PowerUpList';
 import { GameState } from '@/constants/GameState';
 import { CardCountBar } from '@/components/ui/CardCountBar';
 import { RoleCardPicker } from '@/components/ui/RoleCardPicker';
@@ -37,13 +33,6 @@ export default function Game() {
   );
 
   const [enemyAttackCode, setEnemyAttackCode] = useState('');
-  const [isPowerCardOpen, setPowerCardOpen] = React.useState(false);
-  const [PowerUpsIndex, setPowerupsIndex] = React.useState<
-    number | undefined
-  >();
-  const [ingezettePowerupIndex, setIngezettePowerupIndex] = React.useState<
-    number | null
-  >(null);
 
   if (lobby.loading) {
     return;
@@ -99,39 +88,26 @@ export default function Game() {
         </View>
       </View>
 
-      {(stratego.self.isTeamLeader && (
-        <View style={styles.SelectScreenContainer}>
-          <PlayerRole
-            attackCode={stratego.self.attackCode}
-            roleCard={AllRoleCards.find(
-              card => card.id === stratego.self.roleCard,
-            )}
-          />
-          <RoleCardPicker />
-        </View>
-      )) || (
-        <View style={styles.SelectScreenContainer}>
-          <PlayerRole
-            attackCode={stratego.self.attackCode}
-            roleCard={AllRoleCards.find(
-              card => card.id === stratego.self.roleCard,
-            )}
-          />
-        </View>
-      )}
-
-      <View style={styles.PopUpContainer}>
-        {PowerUpsIndex !== undefined && (
-          <PowerUpPopUp
-            name={PowerUpList[PowerUpsIndex].name}
-            description={PowerUpList[PowerUpsIndex].description}
-            image={PowerUpList[PowerUpsIndex].image}
-            onDelete={() => setPowerupsIndex(undefined)}
-            onInzet={() => {
-              setIngezettePowerupIndex(PowerUpsIndex);
-              setPowerupsIndex(undefined);
-            }}
-          />
+      <View>
+        {(stratego.self.isTeamLeader && (
+          <View style={styles.SelectScreenContainer}>
+            <PlayerRole
+              attackCode={stratego.self.attackCode}
+              roleCard={AllRoleCards.find(
+                card => card.id === stratego.self.roleCard,
+              )}
+            />
+            <RoleCardPicker />
+          </View>
+        )) || (
+          <View style={styles.SelectScreenContainer}>
+            <PlayerRole
+              attackCode={stratego.self.attackCode}
+              roleCard={AllRoleCards.find(
+                card => card.id === stratego.self.roleCard,
+              )}
+            />
+          </View>
         )}
       </View>
 
@@ -185,48 +161,6 @@ export default function Game() {
         </View>
       </View>
       {/* Einde test gedeelte gameloop */}
-
-      <Pressable
-        style={{
-          transform: [{ translateY: isPowerCardOpen ? '5%' : '80%' }],
-          ...styles.PowerUpBarContainer,
-        }}
-        onPress={() => setPowerCardOpen(!isPowerCardOpen)}>
-        <View style={styles.PowerCardTitleContainer}>
-          <Text
-            style={{
-              transform: [{ rotate: isPowerCardOpen ? '90deg' : '-90deg' }],
-              ...styles.PowerUpArrow,
-            }}>
-            ➔
-          </Text>
-          <Text style={styles.PowerUpTekst}>Power-ups</Text>
-        </View>
-        <View style={styles.PowerUpCardContainer}>
-          {PowerUpList.map((powerUp, index) => (
-            <Pressable
-              key={powerUp.id}
-              style={[
-                styles.PowerUpPressable,
-                index === ingezettePowerupIndex && {
-                  borderWidth: 6,
-                  borderColor: '#70C25C',
-                  borderRadius: 16,
-                  shadowColor: 'rgba(0, 0, 0, 0.25)',
-                  shadowOffset: { width: 0, height: 8 },
-                  shadowRadius: 8,
-                  shadowOpacity: 8,
-                },
-              ]}
-              onPress={event => {
-                event.stopPropagation();
-                if (isPowerCardOpen) setPowerupsIndex(index);
-              }}>
-              <Image source={powerUp.image} style={styles.PowerupCard} />
-            </Pressable>
-          ))}
-        </View>
-      </Pressable>
     </SafeAreaView>
   );
 }
@@ -243,22 +177,6 @@ const styles = StyleSheet.create({
     paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
     margin: -1.2,
     overflow: 'hidden',
-  },
-  SpelerCard: {
-    width: '70%',
-    height: '40%',
-    borderRadius: 12,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  SpelerCardSelect: {
-    position: 'relative',
-    flexShrink: 0,
-    width: '80%',
-    height: '90%',
-    borderRadius: 12,
-    marginTop: 10,
-    marginLeft: 30,
   },
   BattleLogButtonContainer: {
     position: 'absolute',
@@ -347,101 +265,10 @@ const styles = StyleSheet.create({
     marginTop: -40,
     marginLeft: -39,
   },
-  PowerUpBarContainer: {
-    position: 'absolute',
-    width: '100%',
-    bottom: 0,
-    left: 0,
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: '35%',
-    backgroundColor: 'rgba(194, 123, 92, 1)',
-    borderRadius: 36,
-    transitionDuration: '0.3s',
-  },
-  PowerCardTitleContainer: {
-    height: '20%',
-    width: '100%',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  PowerUpArrow: {
-    color: 'rgba(255, 255, 255, 1)',
-    fontFamily: 'Inter',
-    fontSize: 24,
-    fontWeight: 400,
-    transitionDuration: '0.3s',
-  },
-  PowerUpTekst: {
-    color: 'rgba(255, 255, 255, 1)',
-    fontFamily: 'Inter',
-    fontSize: 24,
-    fontWeight: 400,
-  },
-  PowerUpCardContainer: {
-    height: '80%',
-    width: '100%',
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'center',
-  },
-  PowerUpPressable: {
-    height: '90%',
-    width: '30%',
-  },
-  PowerupCard: {
-    width: '100%',
-    height: '100%',
-    borderRadius: 12,
-    borderWidth: 1,
-    borderColor: 'black',
-  },
   SelectScreenContainer: {
     rowGap: 8,
     width: '80%',
     height: '40%',
     marginBottom: 150,
-  },
-  SelectFieldContainer: {
-    width: '70%',
-    height: '10%',
-    marginTop: 10,
-    marginLeft: 50,
-    color: 'black',
-    backgroundColor: 'white',
-    borderRadius: 12,
-    marginBottom: 10,
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderWidth: 1,
-    borderColor: 'black',
-    padding: 10,
-  },
-  ConfirmButtom: {
-    position: 'relative',
-    flexShrink: 0,
-    width: '90%',
-    height: '10%',
-    backgroundColor: 'rgba(112, 194, 92, 1)',
-    shadowColor: 'rgba(0, 0, 0, 0.25)',
-    shadowRadius: 4,
-    borderRadius: 12,
-    marginTop: 50,
-    marginLeft: 20,
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  opmaakContainer: {
-    display: 'flex',
-    flexDirection: 'column',
-    rowGap: 2,
   },
 });
