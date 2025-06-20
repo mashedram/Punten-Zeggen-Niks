6e040cd61573f358b641c3b481171171c9ecca39
Bas Bakker <basbakker1912@gmail.com>
2025-06-20 12:39:05 +0200

    feat: made popups functional

--- components/stratego/popup/TradeCardPopup.tsx
+++ components/stratego/popup/TradeCardPopup.tsx
@@ -4,14 +4,22 @@ import { View } from 'react-native';
 
 const TradecardPopup = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Trade Card</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    zIndex: 3000,
+  },
 });
 
 export default TradecardPopup;
